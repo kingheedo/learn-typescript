27f0a5b00edb87ecdb92e7cfd9e19190db8f105b
heedo-wang <dhkdgmleh@gmail.com>
2021-01-18 16:23:31 +0900

    유니온 타입과 인터섹션 타입의 차이점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -25,19 +25,23 @@ interface Person{
 name:string;
 age: number;
 }
-function askSomeone(someone:Developer | Person){
+// function askSomeone(someone:Developer | Person){
     //someone에 대해서 유니온타입을 적용했을때 에러 발생 
-    someone.name
-    someone.age
-    someone.skill
+    // someone.name
+    // someone.age
+    // someone.skill
     
-}
+// }
+// askSomeone({name:'디벨로퍼' skill: '웹 개발'});
+// askSomeone({name:'캡틴' age:100 });
+
 function askSomeone(someone:Developer & Person){
     someone.name
     someone.age
     someone.skill
     
 }
+askSomeone({name:'디벨로퍼', skill: '웹 개발', age: 34});
 
 var seho: string | number | boolean;
 var capt: string & number & boolean;
