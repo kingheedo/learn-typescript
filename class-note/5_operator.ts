6468276d452f6a8fc0a4e0d62a9c4bc0b9368ac8
heedo-wang <dhkdgmleh@gmail.com>
2021-01-18 16:01:31 +0900

    연산자를 이용한 타입 정의 - Union Type

---
+++ class-note/5_operator.ts
@@ -0,0 +1,12 @@
+// function logMessage(value: any){
+//     console.log(value);
+// }
+// logMessage('hello');
+// logMessage(100);
+
+
+function logMessage(value: string | number){
+    console.log(value);
+}
+logMessage("hello");
+logMessage(100);
