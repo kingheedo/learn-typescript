c4e3678a5c9fb5cc5775a1a28ac562992a2b70a6
heedo-wang <dhkdgmleh@gmail.com>
2021-01-18 16:10:51 +0900

    유니온 타입의 특징

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -16,3 +16,18 @@ function logMessage(value: string | number){
 }
 logMessage("hello");
 logMessage(100);
+
+interface Developer{
+name:string;
+skill:string;
+}
+interface Person{
+name:string;
+age: number;
+}
+function askSomeone(someone:Developer | Person){
+    //someone에 대해서 유니온타입을 적용했을때 에러 발생 
+    someone.name
+    someone.age
+    
+}
