946ea0169b31753006bdf82482f27fba06018174
heedo-wang <dhkdgmleh@gmail.com>
2021-01-19 18:04:52 +0900

    제네릭 - 코드에 타입 정의하기

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,30 @@
-const emails = [
+interface Email {
+  value: string;
+  selected: boolean;
+}
+
+const emails: Email[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
+interface ProductNumber{
+  value: number;
+  selected: boolean;
+}
 
-const numberOfProducts = [
+interface TrueFalse{
+  value: boolean;
+  selected: boolean;
+}
+
+const numberOfProducts: ProductNumber[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem(item: Email | ProductNumber) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -24,3 +38,10 @@ emails.forEach(function (email) {
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
+
+numberOfProducts.forEach.(function(product) {
+  const item = createDropdownItem(product);
+})
+
+// 언제든지 value 의 타입이 변경 될 수 있다는 가정하에 
+// 수용할 수 있게 interface를 통해서 하는게 아니라 유연하게 갖다 쓸 수 있도록 제네릭을 이용할 수 있을 것이다.
