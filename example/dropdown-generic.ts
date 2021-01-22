f94ff26baa990ab22436b10bfc5f1c1af59c7bfa
heedo-wang <dhkdgmleh@gmail.com>
2021-01-22 23:46:45 +0900

    제네릭을 이용한 타입정의

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,30 +1,26 @@
-interface Email {
-  value: string;
-  selected: boolean;
+interface DropdownItem<T>{
+  value: T;
+  selected : boolean;
 }
 
-const emails: Email[] = [
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
-interface ProductNumber{
-  value: number;
-  selected: boolean;
-}
 
-interface TrueFalse{
-  value: boolean;
-  selected: boolean;
-}
-
-const numberOfProducts: ProductNumber[] = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: Email | ProductNumber) {
+function createDropdownItem<T>(item: DropdownItem<T> | DropdownItem<T>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -34,7 +30,7 @@ function createDropdownItem(item: Email | ProductNumber) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
