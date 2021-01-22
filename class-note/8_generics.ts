531375eb375484b006bc117c4ad3525aa26c6ee5
heedo-wang <dhkdgmleh@gmail.com>
2021-01-23 00:11:32 +0900

    정의된 타입으로 타입을 제한하기

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -59,7 +59,7 @@ interface Dropdown<T>{
 const obj: Dropdown<string> ={value: 'abc', selected: false};
 
 //제네릭의 타입 제한
-function logTextLength<T>(text: T[]): T{
+function logTextLength<T>(text: T[]): T[]{
     console.log(text.length)
     text.forEach(function(text){
         console.log(text);
@@ -68,3 +68,14 @@ function logTextLength<T>(text: T[]): T{
 }
 
 logTextLength<string>(['hi','abc']);
+
+//제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType{
+    length: number;
+}
+function logTextLength<T extends LengthType>(text:T):T{
+    text.length;
+    return text;
+}
+logTextLength(10);
+logTextLength({length:10});
